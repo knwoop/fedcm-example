4fbcf1b8fc8682638c0493e747f4465f1e39e4d7
Kenta Takahashi <knwoop@gmail.com>
2022-12-20 15:44:31 +0900

    feat: adopt styled-compoments

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,49 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// global style
+const GlobalStyle = createGlobalStyle`
+html,
+body,
+textarea {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
+* {
+  box-sizing: border-box;
+}
+a {
+  cursor: pointer;
+  text-decoration: none;
+  transition: .25s;
+}
+
+ol, ul {
+  list-style: none;
+}
+`
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+      <>
+        <Head>
+          <meta key="charset" name="charset" content="utf-8" />
+          <meta
+              key="viewport"
+              name="viewport"
+              content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+          />
+          <meta property="og:locale" content="ja_JP" />
+          <meta property="og:type" content="website" />
+        </Head>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </>
+  )
+}
+
+export default MyApp
