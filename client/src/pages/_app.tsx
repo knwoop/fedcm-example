ad0166ffae5396701a866be652fe5ea9118aac18
Kenta Takahashi <knwoop@gmail.com>
2022-12-20 15:53:32 +0900

    feat: add lint to all project

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
-import Head from 'next/head'
 import type { AppProps } from 'next/app'
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 // global style
 const GlobalStyle = createGlobalStyle`
@@ -26,23 +26,22 @@ ol, ul {
   list-style: none;
 }
 `
-
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-      <>
-        <Head>
-          <meta key="charset" name="charset" content="utf-8" />
-          <meta
-              key="viewport"
-              name="viewport"
-              content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
-          />
-          <meta property="og:locale" content="ja_JP" />
-          <meta property="og:type" content="website" />
-        </Head>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </>
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
   )
 }
 
