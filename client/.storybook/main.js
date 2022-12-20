8dc06c7fa139d79da666d5a84b955cd132a4782a
Kenta Takahashi <knwoop@gmail.com>
2022-12-20 16:35:40 +0900

    feat: add storybook

---
+++ client/.storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "typescript" : { reactDocgen: false },
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
