0657d82e765e28c34f3032f576535cf4079b74aa
Kenta Takahashi <knwoop@gmail.com>
2022-12-26 18:36:32 +0900

    fix: React Hydration Error

--- rp/src/containers/SigninWithFedCMContainer.tsx
+++ rp/src/containers/SigninWithFedCMContainer.tsx
@@ -1,4 +1,5 @@
 import { randomBytes } from 'crypto'
+import { useEffect, useState } from 'react'
 import Button from 'components/atoms/Button'
 import { useAuthContext } from 'contexts/AuthContext'
 import { useGlobalSpinnerActionsContext } from 'contexts/GlobalSpinnerContext'
@@ -8,31 +9,42 @@ interface SigninWithFedCMContainerProps {
 }
 
 /**
- * サインインフォームコンテナ
+ * Sign-in with FedCM container
  */
 const SigninWithFedCMContainer = ({
   onSignin,
 }: SigninWithFedCMContainerProps) => {
-  const { signinWithFedCM } = useAuthContext()
+  const { signinWithFedCM, isFedCMEnabled } = useAuthContext()
+
+  const [mounted, setMounted] = useState(false)
+
   const setGlobalSpinner = useGlobalSpinnerActionsContext()
   // サインインボタンを押された時のイベントハンドラ
   const handleSignin = async () => {
     const N = 16
     const nonce = randomBytes(N).toString('base64').substring(0, N)
     try {
-      // ローディングスピナーを表示する
       setGlobalSpinner(true)
       await signinWithFedCM(nonce)
       onSignin && onSignin()
     } catch (err: unknown) {
       if (err instanceof Error) {
-        // エラーの内容を表示
         window.alert(err.message)
         onSignin && onSignin(err)
       }
     } finally {
       setGlobalSpinner(false)
     }
+  }
+
+  useEffect(() => {
+    if (isFedCMEnabled()) {
+      setMounted(true)
+    }
+  }, [isFedCMEnabled])
+
+  if (!mounted) {
+    return null
   }
 
   return (
